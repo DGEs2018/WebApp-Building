40660220c8b1ae3d790a286557214644ec814f92
Dawit <dawit@ginetta.net>
2020-01-27 10:40:52 +0100

    Create react up, render hello react, render a list of items each containing author, title, url etc., then use map to display the list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+	{
+		title: 'React',
+		url: 'https://reactjs.org',
+		author: 'Jordan Walke',
+		num_comments: 3,
+		points: 4,
+		objectID: 0
+	},
+	{
+		title: 'Redux',
+		url: 'https://redux.js.org/',
+		author: 'Dan Abramov, Andrew Clark',
+		num_comments: 2,
+		points: 5,
+		objectID: 1
+	},
+	{
+		title: 'Context',
+		url: 'https://redux.js.org/',
+		author: 'Whoever',
+		num_comments: 2,
+		points: 3,
+		objectID: 2
+	},
+	{
+		title: 'Layouts',
+		url: 'https://redux.js.org/',
+		author: 'Someone else',
+		num_comments: 2,
+		points: 4,
+		objectID: 3
+	},
+	{
+		title: 'Layouts',
+		url: 'https://redux.js.org/',
+		author: 'Someone else',
+		num_comments: 2,
+		points: 4,
+		objectID: 4
+	}
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const helloReact = 'Welcome to React fundamentals!';
+	return (
+		<div className="App">
+			{/* <h2>{helloReact}</h2> */}
+			{list.map(function(item) {
+				<div key={item.objectID}>
+					<span>
+						<a href={item.url}>{item.title}</a>
+					</span>
+					<span>{item.author}</span>
+					<span>{item.num_comments}</span>
+					<span>{item.points}</span>
+				</div>;
+			})}
+		</div>
+	);
 }
 
 export default App;
